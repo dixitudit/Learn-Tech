bee18a6f0716dcd2f0355fa18d2353c8e904f252
dixitudit <58696687+dixitudit@users.noreply.github.com>
2024-04-05 16:36:05 +0530

    models and controllers

--- api/index.js
+++ api/index.js
@@ -1,19 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    ()=>{
-        console.log('MongoDB is connected')
-    }
-);
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("MongoDB is connected");
+});
 
 const app = express();
 
+app.use('/api/user',userRoutes);
 
 
-app.listen(3000,()=>{
-    console.log('server is running on port number 3000');
-})
+app.listen(3000, () => {
+  console.log("server is running on port number 3000");
+});
---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import { Mongoose } from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mangoose.model("User", userSchema);
+
+export default User;
