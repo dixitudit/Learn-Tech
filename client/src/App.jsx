750c35eff5ab096be6e3bc5ff9855dd05c669b08
dixitudit <58696687+dixitudit@users.noreply.github.com>
2024-04-09 00:46:20 +0530

    Private Route for Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
 // 2:21:16
 
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import FooterComponent from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComponent from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/sign-up' element={<SignUp/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
-        <Route path='/projects' element={<Projects/>}></Route>
-        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
       </Routes>
-      <FooterComponent/>
+      <FooterComponent />
     </>
   );
 }
