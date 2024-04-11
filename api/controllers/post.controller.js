df7f5ecb88b41bf0819c08cc367a4d19fa477bcc
dixitudit <58696687+dixitudit@users.noreply.github.com>
2024-04-11 17:17:21 +0530

    created api route to get posts based on the query

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,24 +1,72 @@
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createPost = async (req,res,next)=>{
-    console.log(req.user);
-    if(!req.user.isAdmin){
-        return next(errorHandler(403, 'Access Denied'));
-    }
-    if(!req.body.title || !req.body.content){
-        return next(errorHandler(400, 'Please provide all required fields'))
-    }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g,'-');
+export const createPost = async (req, res, next) => {
+  console.log(req.user);
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "Access Denied"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
 
-    const newPost = new Post ({
-        ...req.body, slug, userId: req.user.id,
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    console.log(req.query);
+    // here query is something which is written after ? in the url separated by & for this route for example someone call get request for ip/api/posts/getposts?startIndex=0&limit=10&order=asc&category=uncategorized then req.query will be {startIndex: 0, limit: 10, order: 'asc', category: 'uncategorized'}
+
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
     });
-    try{
-        const savedPost = await newPost.save();
-        res.status(201).json(savedPost);
-    }
-    catch(err){
-        return next(err);
-    }
-}
+
+    res.status(200).json({posts, totalPosts, lastMonthPosts});
+  } catch (err) {
+    return next(err);
+  }
+};
